2f30e894b61eb1c2a3fa5c707c7080964a93ef02
Josh de Kock <josh@itanimul.li>
2016-07-30 14:39:02 +0100

    build-pkgs.sh: switch to dependency system
    Allows for packages which have dependencies to fail without breaking
    the whole repo.

--- build-pkgs.sh
+++ build-pkgs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -a packages=(zlib libpng freetype2 libexif libjpeg-turbo sqlite)
+[ -d packages ] && rm -rf packages/*
 
 if [ "${HAS_JOSH_K_SEAL_OF_APPROVAL:-false}" ]; then
     export VITASDK=/usr/local/vitasdk
@@ -11,20 +11,31 @@ if [ "${HAS_JOSH_K_SEAL_OF_APPROVAL:-false}" ]; then
     BINTRAY_PKG_VER=$(printf %04d ${TRAVIS_BUILD_NUMBER})
 fi
 
-for package in "${packages[@]}"
-do
-    ./build-pkg.sh "repo/${package}.desc" || exit 1
+function bintray(){
+    package="$1"
+    if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+        pushd packages
+        pkg=$(find * -name "vitasdk-${package}-*.tar.xz")
+        echo "$pkg" >> packages.list
+        curl -T "$pkg" -ujoshdekock:$BINTRAY_APIKEY "https://api.bintray.com/content/vitadev/dist/ports/${BINTRAY_PKG_VER}/$pkg"
+        popd
+    fi
+}
+
+function build(){
+    package="$1"
+    ./build-pkg.sh "repo/${package}.desc" || { echo "$package" >> packages/broken.list ; return 1; }
     tar xvf packages/vitasdk-${package}*.tar.xz -C $VITASDK
     if [ "${HAS_JOSH_K_SEAL_OF_APPROVAL:-false}" ]; then
-        if [ "$TRAVIS_OS_NAME" == "linux" ]; then
-            pushd packages
-            pkg=$(find * -name "vitasdk-${package}-*.tar.xz")
-            echo "$pkg" >> packages.list
-            curl -T "$pkg" -ujoshdekock:$BINTRAY_APIKEY "https://api.bintray.com/content/vitadev/dist/ports/${BINTRAY_PKG_VER}/$pkg"
-            popd
-        fi
+        bintray "$package"
     fi
-done
+}
+
+# manual dependencies
+build zlib && build libpng && build freetype2
+build libexif && libjpeg-turbo
+build sqlite
+build fftw
 
 if [ "${HAS_JOSH_K_SEAL_OF_APPROVAL:-false}" ]; then
     if [ "$TRAVIS_OS_NAME" == "linux" ]; then
@@ -33,3 +44,5 @@ if [ "${HAS_JOSH_K_SEAL_OF_APPROVAL:-false}" ]; then
         popd
     fi
 fi
+
+[ -f packages/broken.list ] && echo "Broken packages" && cat packages/broken.list
