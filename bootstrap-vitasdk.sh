e29de3219724dd4f651464e069951cd2e8065118
Chris Jones <cmsj@tenshu.net>
2018-01-09 19:21:14 +0000

    boostrap now actually honours VITASDK env var

--- bootstrap-vitasdk.sh
+++ bootstrap-vitasdk.sh
@@ -3,7 +3,7 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-INSTALLDIR="/usr/local/vitasdk"
+INSTALLDIR="${VITASDK:-/usr/local/vitasdk}"
 
 . $DIR/include/install-vitasdk.sh
 
@@ -16,6 +16,6 @@ echo "==> Installing vitasdk to $INSTALLDIR"
 install_vitasdk $INSTALLDIR
 
 echo "Please add the following to the bottom of your .bashrc:"
-printf "\033[0;36m"'export VITASDK=/usr/local/vitasdk'"\033[0m\n"
+printf "\033[0;36m""export VITASDK=${INSTALLDIR}""\033[0m\n"
 printf "\033[0;36m"'export PATH=$VITASDK/bin:$PATH'"\033[0m\n"
 echo "and then restart your terminal"
