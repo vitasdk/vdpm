9afe47538d67efdfedea6b6e83c5027db8a97841
xyzz <1065521+xyzz@users.noreply.github.com>
2018-01-10 19:08:46 -0500

    vitasdk-update: Add a script to update an existing vitasdk installation (#25)
    * vitasdk-update: Add a script to update an existing vitasdk installation
    
    This is to be installed into vitasdk by buildscripts.
    
    The installation flow remains compatible (bootstrap-vitasdk, then install-all)
    but now the user can run vitasdk-update to update the SDK and packages.

---
+++ include/install-packages.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+b() {
+	$DIR/../vdpm $1
+}
+
+install_packages() {
+	b zlib
+	b bzip2
+	b libzip
+	b libpng
+	b libexif
+	b libjpeg-turbo
+	b jansson
+	b yaml-cpp
+	b freetype
+	b harfbuzz
+	b fftw
+	b libvita2d
+	b libmad
+	b libogg
+	b libvorbis
+	b libtremor
+	b libftpvita
+	b henkaku
+	b taihen
+	b libk
+	b libdebugnet
+	b onigmo
+	b sdl
+	b sdl_image
+	b sdl_mixer
+	b sdl_net
+	b sdl_ttf
+	b sdl2
+	b sdl2_image
+	b sdl2_mixer
+	b sdl2_net
+	b sdl2_ttf
+	b openssl
+	b curl
+	b expat
+	b opus
+	b unrar
+	b glm
+	b libxml2
+	b speexdsp
+	b pixman
+	b TinyGL
+	b kuio
+	b taipool
+	b mpg123
+	b soloud
+	b quirc
+	b Box2D
+	b libsndfile
+}
