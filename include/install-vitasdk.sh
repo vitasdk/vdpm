d726af20b4f7879d29f79b362474c89cb8bc321d
Rémy F <yne@users.noreply.github.com>
2020-01-18 21:31:55 +0100

    install curl if not installed
    Shall fix https://github.com/vitasdk/vdpm/issues/51

--- include/install-vitasdk.sh
+++ include/install-vitasdk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 get_download_link () {
-  curl -sL https://github.com/vitasdk/vita-headers/raw/master/.travis.d/last_built_toolchain.py | python - $@
+  wget -qO- https://github.com/vitasdk/vita-headers/raw/master/.travis.d/last_built_toolchain.py | python - $@
 }
 
 install_vitasdk () {
@@ -10,30 +10,25 @@ install_vitasdk () {
   case "$(uname -s)" in
      Darwin*)
       mkdir -p $INSTALLDIR
-      wget -O "vitasdk-nightly.tar.bz2" "$(get_download_link master osx)"
-      tar xf "vitasdk-nightly.tar.bz2" -C $INSTALLDIR --strip-components=1
-      rm -f "vitasdk-nightly.tar.bz2"
+      wget -O- "$(get_download_link master osx)" | tar xj -C $INSTALLDIR --strip-components=1
      ;;
 
      Linux*)
       if [ -n "${TRAVIS}" ]; then
           sudo apt-get install libc6-i386 lib32stdc++6 lib32gcc1 patch
       fi
+      command -v curl || { echo "curl missing (install using: apt install curl)" ; exit 1; }
       if [ ! -d "$INSTALLDIR" ]; then
         sudo mkdir -p $INSTALLDIR
         sudo chown $USER:$(id -gn $USER) $INSTALLDIR
       fi
-      wget -O "vitasdk-nightly.tar.bz2" "$(get_download_link master linux)"
-      tar xf "vitasdk-nightly.tar.bz2" -C $INSTALLDIR --strip-components=1
-      rm -f "vitasdk-nightly.tar.bz2"
+      wget -O- "$(get_download_link master linux)" | tar xj -C $INSTALLDIR --strip-components=1
      ;;
 
      MSYS*|MINGW64*)
       UNIX=false
       mkdir -p $INSTALLDIR
-      wget -O "vitasdk-nightly.tar.bz2" "$(get_download_link master win)"
-      tar xf "vitasdk-nightly.tar.bz2" -C $INSTALLDIR --strip-components=1
-      rm -f "vitasdk-nightly.tar.bz2"
+      wget -O- "$(get_download_link master win)" | tar xj -C $INSTALLDIR --strip-components=1
      ;;
 
      CYGWIN*|MINGW32*)
